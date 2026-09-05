b7be54e3c66cf002c225f3833bec1b435d1b3497
Kinjitou <45107430+Kinjitou@users.noreply.github.com>
2026-09-05 18:53:37 +0900

    Add files via upload
    めめカメラに変更

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lmcam-v1";
+const CACHE = "lmcam-v2";
 const ASSETS = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
